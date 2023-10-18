da98f8b7cda110873259a14aa1bf6ddcd715cb7b
isalotra <isalotra.kikia@gmail.com>
2023-10-18 15:44:28 +0300

    create Letter and Letter Tittle column

--- prepross_1.py
+++ prepross_1.py
@@ -16,3 +16,22 @@ def divide_elements(s: str, replace_su_note=True, min_word_count=75):
         s = re.sub(pattern, '', s, count=1, flags=re.DOTALL)
 
     return str(s), str(su_note)
+
+
+def extractH2(centent:str):
+    motif = r'<h2>(.*?)<\/h2>'
+    return re.findall(motif, centent, re.DOTALL)
+
+def createTitleColumn(reader_csv):
+    for ligne in reader_csv:
+        # Utiliser regex pour extraire le contenu de <h2> de la colonne "Content"
+        content = ligne['Content']
+        resultat_regex = extractH2(content)
+        if resultat_regex:
+            titre = resultat_regex.group(1)
+        else:
+            titre = ''  # Si <h2> n'est pas trouvé, laisser la valeur vide
+
+        # Ajouter la nouvelle valeur à la ligne
+        ligne['Letter Tittle'] = titre
+        ligne['Letter'] = divide_elements(s=content)
